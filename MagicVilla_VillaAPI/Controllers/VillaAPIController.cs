fd17b8e34f25637f75d88511f615a94f8a3be2c8
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-04-22 15:21:37 +0530

    custom validation

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -16,7 +16,7 @@ namespace MagicVilla_VillaAPI.Controllers
 			return Ok(VillaStore.villaList);
 		}
 
-		[HttpGet("{id:int}")]
+		[HttpGet("{id:int}",Name ="GetVilla")]
 		[ProducesResponseType(200)]
 		[ProducesResponseType(400)]
 		[ProducesResponseType(404)]
@@ -33,5 +33,30 @@ namespace MagicVilla_VillaAPI.Controllers
 			}
 			return Ok(villa);
 		}
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+		{
+			if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+			{
+				ModelState.AddModelError("", "Villa already Exist");
+				return BadRequest(ModelState);
+			}
+			if (villaDTO == null)
+			{
+				return BadRequest(villaDTO);
+			}
+			if(villaDTO.Id > 0)
+			{
+				return BadRequest(StatusCodes.Status500InternalServerError);
+			}
+			villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+			VillaStore.villaList.Add(villaDTO);
+
+			return CreatedAtRoute("GetVilla",new {id = villaDTO.Id},villaDTO);
+		}
+
 	}
 }
