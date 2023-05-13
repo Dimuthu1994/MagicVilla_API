76d5536aec574e855670bb4a96ce9c897a49ba33
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-05-13 08:41:39 +0530

    Assigment 1

---
+++ MagicVilla_VillaAPI/Models/DTO/VillaNumberDTO .cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models.DTO
+{
+	public class VillaNumberDTO
+	{
+		[Required]
+		public int VillaNo { get; set; }
+
+		public string SpecialDetails { get; set; }
+	}
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -15,6 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Host.UseSerilog();
 builder.Services.AddDbContext<ApplicationDbContext>(option => { option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
 builder.Services.AddScoped<IVillaRepository,VillaRepository>();
+builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
